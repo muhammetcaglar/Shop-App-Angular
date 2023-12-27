37e2345b46464f33462b65ec41551ad2726f1ed7
Muhammet ÇAĞLAR <Your new email>
2023-12-27 23:00:09 +0300

    Shop basket page prepared

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -7,8 +7,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CardSummaryComponent } from './card-summary/card-summary.component';
 import { CartDetailComponent } from './cart-detail/cart-detail.component';
 import { CheckOutComponent } from './check-out/check-out.component';
+import { RouterModule } from "@angular/router";
 @NgModule({
-  imports:[ModelModule,BrowserModule,FormsModule],
+  imports:[ModelModule,BrowserModule,FormsModule,RouterModule],
   declarations:[ShopComponent, NavbarComponent, CardSummaryComponent, CartDetailComponent, CheckOutComponent],
   exports:[ShopComponent,CartDetailComponent, CheckOutComponent]
 })
